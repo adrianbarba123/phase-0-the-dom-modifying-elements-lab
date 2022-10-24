fcce7cf21993d5dd406e727f25757f65ddf4fd65
Adrian Barba <adrianbarba123@gmail.com>
2022-10-23 18:29:12 -0700

    commit tree f58e46ada3b98de2873c5518cee16ab5cd9ae7bd

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const element = document.createElement("div");
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Adrian is the champion!";
